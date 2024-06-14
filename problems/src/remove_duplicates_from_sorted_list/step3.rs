dfce620e170971bef7e268de26d323590f7f2daf
Yoshiki-Iwasa <58158284+Yoshiki-Iwasa@users.noreply.github.com>
2024-06-14 13:30:38 +0900

    Solve 83

---
+++ problems/src/remove_duplicates_from_sorted_list/step3.rs
@@ -0,0 +1,47 @@
+// Step3
+// 目的: 覚えられないのは、なんか素直じゃないはずなので、そこを探し、ゴールに到達する
+
+// 方法
+// 時間を測りながらもう一度解く
+// 10分以内に一度もエラーを吐かず正解
+// これを3回連続でできたら終わり
+// レビューを受ける
+// 作れないデータ構造があった場合は別途自作すること
+
+/*
+  時間計算量: O(n)
+  空間計算量: O(1)
+*/
+struct Solution;
+
+// Definition for singly-linked list.
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+impl Solution {
+    pub fn delete_duplicates(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut current = &mut head;
+        while let Some(current_node) = current {
+            while let Some(next_node) = &mut current_node.next {
+                if current_node.val == next_node.val {
+                    current_node.next = next_node.next.take();
+                } else {
+                    break;
+                }
+            }
+            current = &mut current_node.next
+        }
+        head
+    }
+}
