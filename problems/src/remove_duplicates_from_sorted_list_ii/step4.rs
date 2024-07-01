b087cd964f2e4916625baf5906b4cd1a50200928
Yoshiki-Iwasa <58158284+Yoshiki-Iwasa@users.noreply.github.com>
2024-07-01 15:14:28 +0900

    Add single loop version

--- problems/src/remove_duplicates_from_sorted_list_ii/step4.rs
+++ problems/src/remove_duplicates_from_sorted_list_ii/step4.rs
@@ -43,4 +43,33 @@ impl Solution {
 
         sentinel.next
     }
+
+    // whileが1重の場合
+    pub fn delete_duplicates_single_loop(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut sentinel = ListNode { val: 0, next: head };
+
+        let mut prev = &mut sentinel;
+
+        let mut is_duplicated = false;
+        while let Some(current_node) = prev.next.as_mut() {
+            if current_node
+                .next
+                .as_ref()
+                .is_some_and(|next_node| next_node.val == current_node.val)
+            {
+                is_duplicated = true;
+                prev.next = current_node.next.take();
+                continue;
+            }
+
+            if is_duplicated {
+                prev.next = current_node.next.take();
+                is_duplicated = false;
+            } else {
+                prev = prev.next.as_mut().unwrap();
+            }
+        }
+
+        sentinel.next
+    }
 }
