d4de52af84b8d31a60adf8e48e75c638945e6669
Yoshiki-Iwasa <58158284+Yoshiki-Iwasa@users.noreply.github.com>
2024-07-04 20:35:32 +0900

    Revert "Merge pull request #17 from Yoshiki-Iwasa/695/max_area_of_island"
    This reverts commit 0784eb5949fc2cc83ee3f9ba587e8cbb3137dded, reversing
    changes made to e47dd08121faf69b07df35abb845384436823ba3.

--- problems/src/lib.rs
+++ problems/src/lib.rs
@@ -4,7 +4,6 @@ pub mod first_unique_character_in_a_string;
 pub mod group_anagrams;
 pub mod intersection_of_two_arrays;
 pub mod kth_largest_element_in_a_stream;
-pub mod max_area_of_island;
 pub mod number_of_islands;
 mod remove_duplicates_from_sorted_list;
 mod remove_duplicates_from_sorted_list_ii;
--- problems/src/max_area_of_island.rs
+++
@@ -1,4 +0,0 @@
-mod step1;
-mod step2;
-mod step3;
-mod step4;
