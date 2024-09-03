b7c40826fb026c2629bebf2a1332c218bbe67cef
Yoshiki-Iwasa <58158284+Yoshiki-Iwasa@users.noreply.github.com>
2024-09-03 14:10:01 +0900

    Add step3

--- problems/src/meeting_rooms/step3.rs
+++ problems/src/meeting_rooms/step3.rs
@@ -9,6 +9,22 @@
 // 作れないデータ構造があった場合は別途自作すること
 
 /*
-  時間計算量:
-  空間計算量:
+  時間計算量: O(N)
+  空間計算量: O(1)
 */
+
+pub struct Solution;
+
+impl Solution {
+    pub fn can_attend_meetings(intervals: Vec<Vec<i32>>) -> bool {
+        let mut sorted_intervals = intervals.clone();
+        sorted_intervals.sort_by_key(|interval| interval[0]);
+
+        sorted_intervals.windows(2).all(|w| {
+            let this_end = w[0][1];
+            let next_start = w[1][0];
+
+            this_end <= next_start
+        })
+    }
+}
