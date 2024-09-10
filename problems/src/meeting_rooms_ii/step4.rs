74f4b6e76b5ce6f4ef3a8a128a20ff98deaccff3
Yoshiki-Iwasa <58158284+Yoshiki-Iwasa@users.noreply.github.com>
2024-09-10 14:29:38 +0900

    Add step4

---
+++ problems/src/meeting_rooms_ii/step4.rs
@@ -0,0 +1,41 @@
+// Step4
+
+pub struct Solution;
+
+#[derive(Clone, Ord, PartialOrd, PartialEq, Eq)]
+enum Event {
+    MeetingEnd,
+    MeetingStart,
+}
+
+impl Solution {
+    pub fn min_meeting_rooms(intervals: Vec<Vec<i32>>) -> i32 {
+        let mut meeting_events = intervals
+            .iter()
+            .map(|interval| {
+                vec![
+                    (interval[0], Event::MeetingStart),
+                    (interval[1], Event::MeetingEnd),
+                ]
+            })
+            .collect::<Vec<_>>()
+            .concat();
+
+        meeting_events.sort();
+
+        let mut required_room_count = 0;
+        let mut using_room = 0;
+
+        for (_, event) in meeting_events {
+            match event {
+                Event::MeetingEnd => using_room -= 1,
+                Event::MeetingStart => {
+                    using_room += 1;
+                    required_room_count = required_room_count.max(using_room);
+                }
+            }
+        }
+
+        required_room_count
+    }
+}
